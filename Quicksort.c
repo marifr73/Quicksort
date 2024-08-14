dc0d99dffd965efb5254d85de323eea782b7f907
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-08-14 17:07:50 -0300

    Update Quicksort.c
    Atualizando código

--- Quicksort.c
+++ Quicksort.c
@@ -7,13 +7,34 @@ typedef struct produtos{
     float preco;
 }Produtos;
 
-void preencher (Produtos * produto);
-
-int main(void){
-    
-    return 0;
+void cadastro (Produtos * produto){
+    int opcao;
+    do{
+    printf("1 - Nome\n2 - Código\n3 - Preço\n4 - Sair\n");
+    scanf("%d", &opcao);
+        switch (opcao){
+        case 1:
+            printf("Informe o nome do produto: ");
+            scanf(" %[^\n]", produto->nome);
+            getchar();
+            break;
+        case 2:
+            printf("Informe o código do produto: ");
+            scanf("%d", &produto->codigo);
+            break;
+        case 3:
+            printf("Informe o preço do produto: ");
+            scanf("%f", &produto->preco);
+            break;;
+        default:
+            printf("Código não encontrado, informe outro!\n");
+            break;
+        }
+    }while(opcao != 4); 
 }
 
-void preencher (Produtos * produto){
-    
+int main(void){
+    Produtos produtos;
+    cadastro(&produtos);
+    return 0;
 }
