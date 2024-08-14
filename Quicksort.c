54309e34971f134eaced97058036ee73b116d9a2
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-08-14 16:48:46 -0300

    Update Quicksort.c

--- Quicksort.c
+++ Quicksort.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct produtos{
+    char nome[20];
+    int codigo;
+    float preco;
+}Produtos;
+
+void preencher (Produtos * produto);
+
 int main(void){
     
     return 0;
+}
+
+void preencher (Produtos * produto){
+    
 }
