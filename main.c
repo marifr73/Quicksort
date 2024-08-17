ce6ebd5e9405553e0895dad94bc2f77fa63f0d21
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-08-17 14:02:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <string.h>
+#include <time.h>
 #include "Quicksort.h"
 
 int main(void) {
@@ -17,9 +18,17 @@ int main(void) {
         produtos = realocaMemoria(produtos, capacidade);
     }
 
+    // Calcular o tempo de execução do QuickSort
+    clock_t inicio = clock();
+
     QuickSort(0, numProdutos - 1, produtos);
 
+    clock_t fim = clock();
+    double tempo_execucao = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
     imprime(produtos, numProdutos);  
+
+    printf("Tempo de execução do QuickSort: %.3f segundos\n", tempo_execucao);
 
     free(produtos);
     return 0;  
