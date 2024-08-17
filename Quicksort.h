91f28a8fca25b40ecaf768871a77cfbad24df3e1
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-08-17 14:36:15 -0300

    Update Quicksort.h

--- Quicksort.h
+++ Quicksort.h
@@ -1,18 +1,34 @@
-#ifndef PRODUTOS
-#define PRODUTOS
+#ifndef PRODUTOS  
+#define PRODUTOS  
 
-typedef struct produtos Produtos;
+/* Definição da struct Produtos que representa um produto com nome, código e preço */
+typedef struct produtos Produtos;  
 
+/* Função que aloca memória para um vetor de produtos com a capacidade de 150 produtos.  
+    Retorna um ponteiro para o vetor de Produtos alocado.  */
 Produtos* alocaMemoria(int capacidade);
 
-Produtos* realocaMemoria(Produtos *produtos, int novaCapacidade);
+/* Função que realoca a memória de um vetor de produtos existente para uma nova capacidade.  
+    Retorna um ponteiro para o vetor de Produtos realocado. */ 
+Produtos* realocaMemoria(Produtos *produtos, int novaCapacidade);  
 
-void cadastro(Produtos *produtos, int *numProdutos);
+/* Função que permite o cadastro de produtos. Solicita ao usuário as informações necessárias  
+    para o cadastro dos produtos e atualiza o contador de produtos cadastrados. */ 
+void cadastro(Produtos *produtos, int *numProdutos);  
 
-int particiona(int esquerda, int direita, Produtos *produtos);
+/* Função que divide o vetor de produtos com base no critério de ordenação (nomes dos produtos).  
+    Utiliza o último elemento como pivô e organiza os produtos no vetor.  
+    Retorna o índice do pivô após a divisão. */
+int divisao(int esquerda, int direita, Produtos *produtos);  
 
-void imprime(Produtos *produtos, int numProdutos);
+/* Função que imprime a lista de produtos cadastrados, mostrando nome, código e preço de cada um.  
+    Recebe o vetor de produtos e o número total de produtos. */ 
+void imprime(Produtos *produtos, int numProdutos);  
 
-void QuickSort(int inicio, int final, Produtos *produtos);
+/* Função que realiza o algoritmo de ordenação QuickSort.  
+    Ordena o vetor de produtos entre os índices 'inicio' e 'final' usando recursão. */ 
+void QuickSort(int inicio, int final, Produtos *produtos);  
 
-#endif
+/* Recursão: O quicksort faz chamadas recursivas para duas partes do vetor, utilizando o índice do pivô retornado pela função de partição. Isso ocorre até que todos os elementos estejam ordenados.*/
+
+#endif /* PRODUTOS */
